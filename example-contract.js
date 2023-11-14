278d292d208c94c222e3f5485aef9b1557b97bf8
Andrew Hathaway <hathbanger.ah@gmail.com>
2023-11-14 07:46:57 -0600

    build toolchain w webpack

--- example-contract.js
+++ example-contract.js
@@ -1,39 +1,11 @@
-main();
-
-function main() {
-    const { applicationInput } = parseContractInput();
-    const { contractFn, amount, recipients } = applicationInput;
-
+const start = input => {
+    const { contractFn, amount, recipients } = input;
     const methods = {
         'splitEvenly': {
             transactions: recipients.map(recipient => ({ recipient, amount: Math.floor(amount / recipients.length) }))
         }
     }
-
-    return sendOutput(methods[contractFn])
+    return methods[contractFn]
 }
 
-
-function parseContractInput() {
-    const chunkSize = 1024;
-    const inputChunks = [];
-    let totalBytes = 0;
-    const stdInBuffer = new Uint8Array(chunkSize);
-    const fdIn = 0;
-    const bytesRead = Javy.IO.readSync(fdIn, stdInBuffer);
-    totalBytes += bytesRead;
-    inputChunks.push(stdInBuffer.subarray(0, bytesRead));
-    const { finalBuffer } = inputChunks.reduce((context, chunk) => {
-        context.finalBuffer.set(chunk, context.bufferOffset);
-        context.bufferOffset += chunk.length;
-        return context;
-    }, { bufferOffset: 0, finalBuffer: new Uint8Array(totalBytes) });
-    return JSON.parse(new TextDecoder().decode(finalBuffer))
-}
-
-function sendOutput(output){
-    const encodedOutput = new TextEncoder().encode(JSON.stringify(output));
-    const stdOutBuffer = new Uint8Array(encodedOutput);
-    const fd = 1;
-    Javy.IO.writeSync(fd, stdOutBuffer);
-}
+export default start;
