42df044e119527a7a8861ada6dfdd24f90c9ab45
Andrew Hathaway <hathbanger.ah@gmail.com>
2024-01-24 10:49:10 -0600

    jsdocs

--- lib/contracts/Contract.ts
+++ lib/contracts/Contract.ts
@@ -1,15 +1,37 @@
 import { ContractInput } from '../../types'
 
+/**
+ * Class representing a Contract with methods to manage and execute contract strategies.
+ */
 export class Contract {
+  /**
+   * A dictionary mapping method names to their corresponding strategy functions.
+   * @type {{ [key: string]: Function }}
+   */
   methodStrategies: { [key: string]: Function }
+
+  /**
+   * Constructs a new Contract instance.
+   */
   constructor() {
     this.methodStrategies = {}
   }
 
+  /**
+   * Initiates the execution of a contract method based on the provided input.
+   * @param {ContractInput} input - The input data required to execute a contract method.
+   * @returns The result of executing the contract method.
+   */
   start(input: ContractInput) {
     return this.executeMethod(input)
   }
 
+  /**
+   * Executes a contract method strategy based on the given input.
+   * @param {ContractInput} input - The input data for the contract method, including account information and method-specific inputs.
+   * @throws Will throw an error if the method name specified in `input` is not found in `methodStrategies`.
+   * @returns The result of the strategy execution.
+   */
   executeMethod(input: ContractInput) {
     const { accountInfo, function: fn, inputs } = input
     const strategy = this.methodStrategies[fn]
@@ -21,6 +43,14 @@ export class Contract {
     throw new Error(`Unknown method: ${fn}`)
   }
 
+  /**
+   * Adds a new strategy function to `methodStrategies` or extends an existing one.
+   * @param {string} methodName - The name of the method for which the strategy is defined.
+   * @param {Function} newStrategyFn - The new strategy function to be added or used for extending.
+   * @param {boolean} [extend=false] - Indicates whether the new strategy should extend an existing strategy (if any).
+   * @description If `extend` is true and a strategy exists for `methodName`, the existing strategy is called first,
+   *              and its result is passed to `newStrategyFn` along with the original parameters.
+   */
   addOrExtendMethodStrategy(
     methodName: string,
     newStrategyFn: Function,
--- lib/contracts/FungibleTokenContract.ts
+++ lib/contracts/FungibleTokenContract.ts
@@ -1,12 +1,42 @@
 import { Contract } from './Contract'
 import { AccountInfo, ContractInput, Inputs } from '../../types'
 
+/**
+ * Class representing a fungible token contract, extending the base `Contract` class.
+ * It encapsulates the core functionality and properties of a fungible token.
+ */
 export class FungibleTokenContract extends Contract {
+  /**
+   * The name of the contract.
+   * @type {string}
+   */
   CONTRACT_NAME: string
+
+  /**
+   * The symbol of the contract.
+   * @type {string}
+   */
   CONTRACT_SYMBOL: string
+
+  /**
+   * The number of decimals for the token.
+   * @type {number}
+   */
   CONTRACT_DECIMALS: number
+
+  /**
+   * The total supply of the token.
+   * @type {number}
+   */
   CONTRACT_TOTAL_SUPPLY: number
 
+  /**
+   * Constructs a new instance of the FungibleTokenContract class.
+   * @param {string} name - The name of the token.
+   * @param {string} symbol - The symbol of the token.
+   * @param {number} decimals - The number of decimals for the token.
+   * @param {number} totalSupply - The total supply of the token.
+   */
   constructor(
     name: string,
     symbol: string,
@@ -31,27 +61,55 @@ export class FungibleTokenContract extends Contract {
     }
   }
 
+  /**
+   * Retrieves the contract name.
+   * @returns An object containing the contract name and a success flag.
+   */
   name() {
     return { name: this.CONTRACT_NAME, success: true }
   }
 
+  /**
+   * Retrieves the contract symbol.
+   * @returns An object containing the contract symbol and a success flag.
+   */
   symbol() {
     return { symbol: this.CONTRACT_SYMBOL, success: true }
   }
 
+  /**
+   * Retrieves the number of decimals for the token.
+   * @returns An object containing the number of decimals and a success flag.
+   */
   decimals() {
     return { decimals: this.CONTRACT_DECIMALS, success: true }
   }
 
+  /**
+   * Retrieves the total supply of the token.
+   * @returns An object containing the total supply and a success flag.
+   */
   totalSupply() {
     return { totalSupply: this.CONTRACT_TOTAL_SUPPLY, success: true }
   }
 
+  /**
+   * Retrieves the balance of a given account.
+   * @param {AccountInfo} _ - Account information (unused in this context).
+   * @param {Inputs} input - The input parameters, including balance and owner ID.
+   * @returns An object containing the balance and a success flag.
+   */
   balanceOf(_: AccountInfo, input: Inputs) {
     const { balance, ownerId } = input
     return { address: ownerId, balance: balance ?? 0, success: true }
   }
 
+  /**
+   * Retrieves the allowance for a given spender.
+   * @param {AccountInfo} _ - Account information (unused in this context).
+   * @param {Inputs} input - The input parameters, including allowances and spender.
+   * @returns An object containing the allowance and a success flag.
+   */
   allowance(_: AccountInfo, input: Inputs) {
     const { allowances, spender } = input
     if (!allowances) return { error: 'allowances not found', success: false }
@@ -64,6 +122,12 @@ export class FungibleTokenContract extends Contract {
     }
   }
 
+  /**
+   * Approves a spender to withdraw from an account up to a certain amount.
+   * @param {AccountInfo} _ - Account information (unused in this context).
+   * @param {Inputs} input - The input parameters, including spender, amount, and approvals.
+   * @returns An object containing the updated approvals and a success flag.
+   */
   approve(_: AccountInfo, input: Inputs) {
     const { spender, amount, approvals } = input
     if (!approvals) return { error: 'approvals not found', success: false }
@@ -75,6 +139,12 @@ export class FungibleTokenContract extends Contract {
     return { approvals, success: true }
   }
 
+  /**
+   * Transfers a specified amount of tokens to a specified recipient.
+   * @param {AccountInfo} _ - Account information (unused in this context).
+   * @param {Inputs} input - The input parameters, including amount, owner address, balance, recipient address, and recipient balance.
+   * @returns An object containing the updated balances and a success flag.
+   */
   transfer(_: AccountInfo, input: Inputs) {
     const {
       amount,
