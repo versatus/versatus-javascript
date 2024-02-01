c6509a24bb9fb7c27806b4cbbe6ce61f80297cbb
Andrew Hathaway <hathbanger.ah@gmail.com>
2024-02-01 09:39:32 -0600

    examples

---
+++ example-contract.ts
@@ -0,0 +1,9 @@
+import { FungibleTokenContract } from './lib/classes/contracts/FungibleTokenContract';
+import { Inputs } from './lib';
+
+const start = (input: Inputs) => {
+  const contract = new FungibleTokenContract()
+  return contract.start(input)
+}
+
+export default start
