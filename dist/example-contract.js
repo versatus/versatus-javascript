53ce32e78939c35ffdad7b673a33a687ccb7e799
Andrew Hathaway <hathbanger.ah@gmail.com>
2024-02-02 14:42:28 -0600

    gitignore

---
+++ dist/example-contract.js
@@ -0,0 +1,6 @@
+import { FungibleTokenContract } from './lib/classes/contracts/FungibleTokenContract.js';
+const start = (input) => {
+    const contract = new FungibleTokenContract();
+    return contract.start(input);
+};
+export default start;
--- example-contract.ts
+++
@@ -1,9 +0,0 @@
-import { FungibleTokenContract } from './lib/classes/contracts/FungibleTokenContract';
-import { Inputs } from './lib';
-
-const start = (input: Inputs) => {
-  const contract = new FungibleTokenContract()
-  return contract.start(input)
-}
-
-export default start
