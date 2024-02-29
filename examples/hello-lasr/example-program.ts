b120be8c3760a72b2b360a8579ccdce15b375319
Mike Hostetler <mike.hostetler@siriusxm.com>
2024-02-29 08:07:15 -0700

    First pass at Hello World program

--- examples/hello-lasr/example-program.ts
+++ examples/hello-lasr/example-program.ts
@@ -1,11 +1,46 @@
-import { Program } from '@/lib/classes'
+import { AddressOrNamespace, Outputs, Program, ProgramUpdate, TokenOrProgramUpdate } from '@/lib/classes'
 
 import { ComputeInputs } from '@/lib'
+import { buildProgramUpdateField, buildUpdateInstruction } from '@/lib/builders'
+import { THIS } from '@/lib/consts'
 
 class HelloLasrProgram extends Program {
   constructor() {
     super()
-    Object.assign(this.methodStrategies, {})
+    Object.assign(this.methodStrategies, {
+      hello: this.hello.bind(this),
+    })
+  }
+
+  hello(computeInputs: ComputeInputs) {
+    const { transaction } = computeInputs
+    const { transactionInputs: txInputStr } = transaction
+    const txInputs = JSON.parse(txInputStr)
+    const name = txInputs?.name ?? 'World'
+
+    const currentTime = new Date().getTime()
+    const helloWorldUpdate = buildProgramUpdateField({
+      field: 'data',
+      value: JSON.stringify({
+        hello: `Hello, ${name}! The time is ${currentTime}!`,
+      }),
+      action: 'extend',
+    })
+
+    if (helloWorldUpdate instanceof Error) {
+      throw helloWorldUpdate 
+    }
+
+    const programUpdates = [helloWorldUpdate]
+
+    const helloWorldUpdateInstruction = buildUpdateInstruction({
+      update: new TokenOrProgramUpdate(
+        'programUpdate',
+        new ProgramUpdate(new AddressOrNamespace(THIS), programUpdates)
+      ),
+    })
+
+    return new Outputs(computeInputs, [helloWorldUpdateInstruction]).toJson()
   }
 }
 
