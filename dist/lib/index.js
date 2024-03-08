1a14a160972ce9249bc9a40812818324479e2365
Andrew Hathaway <hathbanger.ah@gmail.com>
2024-03-07 17:11:54 -0700

    imports n stuff

--- dist/lib/index.js
+++ dist/lib/index.js
@@ -1,7 +1,8 @@
 import * as versatus from './versatus.js';
-import * as programs from './programs.js';
+import * as programs from './programs/index.js';
 import * as utils from './utils.js';
 import * as types from './types.js';
-export { LASR_RPC_URL, ETH_PROGRAM_ADDRESS, VIPFS_ADDRESS, VERSE_PROGRAM_ADDRESS, THIS, ZERO_VALUE, } from './consts.js';
+import * as consts from './consts.js';
 export { programs, utils, types };
 export { versatus };
+export { consts };
