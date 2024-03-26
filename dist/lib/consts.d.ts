582be3741586b6041912f10f020608f06aec30ee
Andrew Hathaway <hathbanger.ah@gmail.com>
2024-03-26 13:39:03 +0530

    const stuff

--- dist/lib/consts.d.ts
+++ dist/lib/consts.d.ts
@@ -5,5 +5,5 @@ export declare const LASR_RPC_URL_STABLE = "http://lasr-sharks.versatus.io:9292"
 export declare const VIPFS_URL_UNSTABLE = "147.182.235.126:5001";
 export declare const VIPFS_URL = "167.99.20.121:5001";
 export declare const ETH_PROGRAM_ADDRESS = "0x0000000000000000000000000000000000000000";
-export declare const VERSE_PROGRAM_ADDRESS = "0x5a04fc39f65ab766337123e22d1b571ca0b16318";
+export declare const VERSE_PROGRAM_ADDRESS = "0x0115762bccca7ff5b847ba618a57aafbb9b76245";
 export declare const FAUCET_URL = "https://faucet.versatus.io/";
