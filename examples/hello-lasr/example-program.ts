4dba5220e4f24026b50312a305fcdcd84875d645
Andrew Hathaway <hathbanger.ah@gmail.com>
2024-04-30 14:54:56 +0100

    hello lasr

--- examples/hello-lasr/example-program.ts
+++ examples/hello-lasr/example-program.ts
@@ -5,19 +5,107 @@ import {
   ProgramUpdate,
 } from '@versatus/versatus-javascript/lib/programs/Program'
 import {
+  buildCreateInstruction,
   buildProgramUpdateField,
+  buildTokenDistributionInstruction,
   buildUpdateInstruction,
 } from '@versatus/versatus-javascript/lib/programs/instruction-builders/builder-helpers'
 import { TokenOrProgramUpdate } from '@versatus/versatus-javascript/lib/programs/Token'
 import { AddressOrNamespace } from '@versatus/versatus-javascript/lib/programs/Address-Namespace'
 import { Outputs } from '@versatus/versatus-javascript/lib/programs/Outputs'
+import {
+  getCurrentSupply,
+  parseMetadata,
+  parseTxInputs,
+  validate,
+  validateAndCreateJsonString,
+} from '../../src'
 
 class HelloLasrProgram extends Program {
   constructor() {
     super()
-    Object.assign(this.methodStrategies, {
-      hello: this.hello.bind(this),
-    })
+    this.registerContractMethod('create', this.create)
+    this.registerContractMethod('hello', this.hello)
+  }
+
+  create(computeInputs: ComputeInputs) {
+    try {
+      const { transaction } = computeInputs
+      const { from } = transaction
+      const txInputs = parseTxInputs(computeInputs)
+      let currSupply = getCurrentSupply(computeInputs)
+
+      // metadata
+      const metadata = parseMetadata(computeInputs)
+      const { initializedSupply, totalSupply } = metadata
+      const recipientAddress = txInputs?.to ?? transaction.to
+
+      // data
+      const imgUrl =
+        'https://pbs.twimg.com/profile_images/1765199894539583488/RUiZn7jT_400x400.jpg'
+      const currentSupply = (
+        currSupply + parseInt(initializedSupply)
+      ).toString()
+
+      const methods = 'create,hello'
+      const metadataStr = validateAndCreateJsonString(metadata)
+
+      const addProgramMetadata = buildProgramUpdateField({
+        field: 'metadata',
+        value: metadataStr,
+        action: 'extend',
+      })
+
+      const dataValues = {
+        type: 'hello-lasr',
+        imgUrl,
+        currentSupply,
+        methods,
+      } as Record<string, string>
+
+      const dataStr = validateAndCreateJsonString(dataValues)
+
+      const addProgramData = buildProgramUpdateField({
+        field: 'data',
+        value: dataStr,
+        action: 'extend',
+      })
+
+      const programUpdateInstructions = buildUpdateInstruction({
+        update: new TokenOrProgramUpdate(
+          'programUpdate',
+          new ProgramUpdate(new AddressOrNamespace(THIS), [
+            addProgramMetadata,
+            addProgramData,
+          ])
+        ),
+      })
+
+      const distributionInstruction = buildTokenDistributionInstruction({
+        programId: THIS,
+        initializedSupply,
+        currentSupply,
+        to: recipientAddress,
+        nonFungible: true,
+      })
+
+      const createInstruction = buildCreateInstruction({
+        from,
+        totalSupply,
+        initializedSupply,
+        programId: THIS,
+        programOwner: from,
+        programNamespace: THIS,
+        distributionInstruction,
+      })
+
+      return new Outputs(computeInputs, [
+        createInstruction,
+        programUpdateInstructions,
+      ]).toJson()
+    } catch (e) {
+      throw e
+    }
   }
 
   hello(computeInputs: ComputeInputs) {
